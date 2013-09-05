127796c24807806270fddcc1a436b252fed75719
llxnking <ksk_op@163.com>
2013-09-05 09:21:42 +0800

    first time

---
+++ TestProject/src/com/siriuser/weibocrawl/Constants.java
@@ -0,0 +1,33 @@
+package com.siriuser.weibocrawl;
+
+
+public interface Constants {
+	
+	public final static String SOLR_SERVER_URL = "http://localhost:8090/solr";
+	
+	//SOLR涓�鎻愪氦鐨勬渶澶ф暟閲�	public final static int SOLR_MAX_COMMIT_CNT = 1000;
+	
+	public final static String FIELD_ID = "id";
+	public final static String FIELD_TREND = "2";
+	public final static String FIELD_URL ="url";
+	public final static String FIELD_CONTENT ="content";
+	public final static String FIELD_TITLE ="title";
+	public final static String FIELD_TSTAMP ="tstamp";
+	public final static String FIELD_PUBLISHDATE ="publishedDate";
+	public final static String FIELD_DIGEST ="digest";
+	
+	public final static String XPATH_FETCHER = "//fetcher";
+	
+	public final static int POSITIVE_NATURE = 1; // 姝ｉ潰鏂伴椈
+	public final static int NEUTRAL_NATURE = 2; // 涓�鏂伴椈
+	public final static int NEGATIVE_NATURE = 4; // 璐熼潰鏂伴椈
+	
+	public static final String POS_WORDS_PATH = "conf/pos_words";
+	public static final String NEG_WORDS_PATH = "conf/neg_words";
+	public static final String REL_WORDS_PATH = "conf/ref_words";
+	
+	
+	public static final int SOLR_MAX_COMMIT_CNT=10;
+	
+
+}
---
+++ TestProject/src/testPackage/TestClass.java
@@ -0,0 +1,7 @@
+package testPackage;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("this is the test app");
+	}
+}
